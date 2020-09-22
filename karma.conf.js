cea1882eceff9049dde30935d7d2ba8c9c73142c
svikramjeet <svikramjeet@vmware.com>
2020-09-21 22:19:56 -0700

    Added test options

--- karma.conf.js
+++ karma.conf.js
@@ -25,9 +25,9 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
     customLaunchers: {
-      ChromeHeadless: {
+      ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
